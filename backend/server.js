8bbedbc9521f2b71579f64cfa128f9798130ac38
gersonkaitor <gersonkaitor@gmail.com>
2022-12-03 18:24:22 +0800

    basic express server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('Server...');
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/', (req,res) =>{
+    res.status(200).json({message : 'Hi, Im on port 5000'})
+})
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))

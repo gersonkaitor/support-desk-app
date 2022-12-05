e0a578bec3cd619f90604a3be16764ada276b668
gersonkaitor <gersonkaitor@gmail.com>
2022-12-05 10:44:21 +0800

    error and exception handling

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000
+const app = express();
 
-const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req,res) =>{
-    res.status(200).json({message : 'Hi, Im on port 5000'})
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hi, Im on port 5000" });
+});
 
 //Routes
-app.use('/api/users', require('./routes/userRoutes'))
+app.use("/api/users", require("./routes/userRoutes"));
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));

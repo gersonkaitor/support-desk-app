3de3e5da9e9d3157da8e8f2223116a82f41c370f
gersonkaitor <gersonkaitor@gmail.com>
2022-12-07 10:02:53 +0800

    ticket model and routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 // @desc   Register a new user
-// @route  /api/user
+// @route  /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -49,7 +49,7 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 // @desc   Login a user
-// @route  /api/user/login
+// @route  /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -71,7 +71,7 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 // @desc   Get current user
-// @route  /api/user/me
+// @route  /api/users/me
 // @access Private
 const getMe = asyncHandler(async (req, res) => {
   const user = {

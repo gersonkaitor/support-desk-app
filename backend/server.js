ee3901f3250ab52be93058191d5e1bae1201f061
gersonkaitor <gersonkaitor@gmail.com>
2022-12-08 19:14:14 +0800

    remove errors

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require("express");
 const colors = require('colors')
 const dotenv = require("dotenv").config();
@@ -14,12 +15,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hi, Im on port 5000" });
+  res.status(200).json({ message: "Welcome to Suppot Desk APP" });
 });
 
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
+
+//Serve Frontend
+// if(process.env.NODE_ENV === 'production'){
+//   //Set build folder as static
+//   app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+//   app.get('*',(req,res) => res.sendFile(__dirname, '../','frontend', 'build', 'index.html'))
+// }else{
+//   app.get("/", (req, res) => {
+//     res.status(200).json({ message: "Welcome to Suppot Desk APP" });
+//   });
+// }
 
 app.use(errorHandler);
 

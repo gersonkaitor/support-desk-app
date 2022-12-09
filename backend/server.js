c22bdb524fa57f605f47556f82e0323b0f27544d
Gerson Kaitor <98581472+gersonkaitor@users.noreply.github.com>
2022-12-09 11:03:08 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to Support Desk APP" });
-});
+// app.get("/", (req, res) => {
+//   res.status(200).json({ message: "Welcome to Support Desk APP" });
+// });
 
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
